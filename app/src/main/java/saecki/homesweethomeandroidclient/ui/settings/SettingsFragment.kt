28f0fe3aaa3666aebdf791e4130d2b143056c525
Tobias <tobi@localhost.localdomain>
2019-10-28 16:23:22 +0100

    added settings fragment

---
+++ app/src/main/java/saecki/homesweethomeandroidclient/ui/settings/SettingsFragment.kt
@@ -0,0 +1,13 @@
+package saecki.homesweethomeandroidclient.ui.settings
+
+import android.os.Bundle
+import androidx.preference.PreferenceFragmentCompat
+import saecki.homesweethomeandroidclient.R
+
+class SettingsFragment() : PreferenceFragmentCompat() {
+
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.preferences, rootKey)
+    }
+
+}
