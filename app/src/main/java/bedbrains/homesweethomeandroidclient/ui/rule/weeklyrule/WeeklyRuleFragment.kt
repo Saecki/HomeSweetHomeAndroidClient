547fe8233bcc42ea654169c90abb13f6114f60a3
Saecki <tobiasschmitz2001@gmail.com>
2019-12-01 20:20:00 +0100

    worked on advanced weekly rule view

--- app/src/main/java/bedbrains/homesweethomeandroidclient/ui/rule/weeklyrule/WeeklyRuleFragment.kt
+++ app/src/main/java/bedbrains/homesweethomeandroidclient/ui/rule/weeklyrule/WeeklyRuleFragment.kt
@@ -2,7 +2,6 @@ package bedbrains.homesweethomeandroidclient.ui.rule.weeklyrule
 
 import android.graphics.Color
 import android.os.Bundle
-import android.util.Log
 import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
@@ -12,12 +11,13 @@ import android.widget.Space
 import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.constraintlayout.widget.ConstraintSet
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
-import bedbrains.homesweethomeandroidclient.MainActivity
 import bedbrains.homesweethomeandroidclient.R
-import bedbrains.homesweethomeandroidclient.ui.animation.RiseDownAnimation
 import bedbrains.homesweethomeandroidclient.ui.cardview.EditTimeSpan
+import bedbrains.shared.datatypes.rules.WeeklyTime
+import bedbrains.shared.datatypes.rules.WeeklyTimeSpan
 import java.time.DayOfWeek
 import java.time.LocalTime
 import java.time.format.DateTimeFormatter
@@ -25,61 +25,73 @@ import java.time.format.FormatStyle
 import java.time.format.TextStyle
 import java.time.temporal.WeekFields
 import java.util.*
-import kotlin.collections.ArrayList
 
 class WeeklyRuleFragment : Fragment() {
 
     lateinit var weeklyRuleViewModel: WeeklyRuleViewModel
-    lateinit var rule: LinearLayout
-    lateinit var ruleName: TextView
+    lateinit var constraintSet: ConstraintSet
     lateinit var dayHeader: LinearLayout
     lateinit var daySpace: View
     lateinit var days: MutableList<TextView>
     lateinit var timeLayout: ConstraintLayout
     lateinit var timeLine: LinearLayout
+    lateinit var timeSpanAnchor: Space
     lateinit var times: MutableList<TextView>
     lateinit var horizontalGuideLines: MutableList<View>
     lateinit var verticalGuideLines: MutableList<View>
+    lateinit var timeIndicatorHead: View
+    lateinit var timeIndicatorLine: View
     lateinit var weekdayStrings: ArrayList<String>
     lateinit var locale: Locale
     lateinit var wf: WeekFields
     lateinit var firstDayOfWeek: DayOfWeek
     var timeSpans = mutableListOf<View>()
     var hourHeight = 0
-    var lineWidth = 0
+    var cardViewHandleDiameter = 0
+    var guidelineWidth = 0
+    var indicatorLineWidth = 0
+    var indicatorHeadDiameter = 0
     var textMargin = 0
-    var smallTextMargin = 0
     var gridMargin = 0
     var timeSpanMargin = 0
+    var timeSpanBigMargin = 0
     var headerPadding = 0
-    var elevation = 0f
+    var cardViewElevation = 0f
+    var cardViewHighElevation = 0f
+    var timeIndicatorElevation = 0f
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        weeklyRuleViewModel = ViewModelProviders.of(this).get(WeeklyRuleViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_weekly_rule, container, false)
-        rule = root.findViewById(R.id.rule)
-        ruleName = rule.findViewById(R.id.name)
+        weeklyRuleViewModel = ViewModelProviders.of(this).get(WeeklyRuleViewModel::class.java)
+        constraintSet = ConstraintSet()
         dayHeader = root.findViewById(R.id.day_header) as LinearLayout
+        daySpace = View(context)
+        days = mutableListOf<TextView>()
         timeLayout = root.findViewById(R.id.time_layout) as ConstraintLayout
+        timeSpanAnchor = Space(context)
         timeLine = LinearLayout(context)
         times = mutableListOf<TextView>()
-        days = mutableListOf<TextView>()
         horizontalGuideLines = mutableListOf<View>()
         verticalGuideLines = mutableListOf<View>()
+        timeIndicatorHead = View(context)
+        timeIndicatorLine = View(context)
         weekdayStrings = arrayListOf<String>()
         locale = Locale.getDefault()
         wf = WeekFields.of(locale)
         firstDayOfWeek = wf.firstDayOfWeek
         hourHeight = resources.getDimensionPixelSize(R.dimen.weekly_rule_hour_height)
-        lineWidth = resources.getDimensionPixelSize(R.dimen.line_width)
+        cardViewHandleDiameter = resources.getDimensionPixelSize(R.dimen.card_view_handle_diameter)
+        guidelineWidth = resources.getDimensionPixelSize(R.dimen.guideline_width)
+        indicatorLineWidth = resources.getDimensionPixelSize(R.dimen.time_indicaotr_line_width)
+        indicatorHeadDiameter = resources.getDimensionPixelSize(R.dimen.time_indicator_head_diameter)
         textMargin = resources.getDimensionPixelSize(R.dimen.text_margin)
-        smallTextMargin = resources.getDimensionPixelSize(R.dimen.small_text_margin)
         gridMargin = resources.getDimensionPixelSize(R.dimen.grid_margin)
         timeSpanMargin = resources.getDimensionPixelSize(R.dimen.weekly_rule_time_span_margin)
+        timeSpanBigMargin = resources.getDimensionPixelSize(R.dimen.weekly_rule_time_span_big_margin)
         headerPadding = resources.getDimensionPixelSize(R.dimen.header_padding)
-        elevation = resources.getDimension(R.dimen.card_view_elevation)
-
-        ruleName.text = weeklyRuleViewModel.rule.name
+        cardViewElevation = resources.getDimension(R.dimen.card_view_elevation)
+        cardViewHighElevation = resources.getDimension(R.dimen.card_view_high_elevation)
+        timeIndicatorElevation = resources.getDimension(R.dimen.time_indicator_elevation)
 
         //adding weekday strings
         for (i in DayOfWeek.values().indices) {
@@ -87,7 +99,6 @@ class WeeklyRuleFragment : Fragment() {
         }
 
         //days
-        daySpace = View(context)
         val daySpaceParams = LinearLayout.LayoutParams(0, LinearLayout.LayoutParams.MATCH_PARENT, 0f)
         daySpaceParams.setMargins(0, 0, gridMargin, 0)
         daySpace.layoutParams = daySpaceParams
@@ -105,6 +116,11 @@ class WeeklyRuleFragment : Fragment() {
         }
 
         //times
+        timeLayout.maxHeight = hourHeight * 24
+
+        timeSpanAnchor.id = View.generateViewId()
+        timeLayout.addView(timeSpanAnchor)
+
         timeLine.id = View.generateViewId()
         timeLine.orientation = LinearLayout.VERTICAL
 
@@ -154,10 +170,21 @@ class WeeklyRuleFragment : Fragment() {
             verticalGuideLines.add(guideline)
         }
 
-        //constraining views
-        val constraintSet = ConstraintSet()
+        //time indicator
+        timeIndicatorHead.id = View.generateViewId()
+        timeIndicatorHead.background = ContextCompat.getDrawable(context!!, R.drawable.time_indicator_head)
+        timeIndicatorHead.elevation = timeIndicatorElevation
+        timeLayout.addView(timeIndicatorHead)
+
+        timeIndicatorLine.id = View.generateViewId()
+        timeIndicatorLine.background = ContextCompat.getDrawable(context!!, R.drawable.time_indicator_line)
+        timeIndicatorLine.elevation = timeIndicatorElevation
+        timeLayout.addView(timeIndicatorLine)
 
         //constraining timeline
+        constraintSet.connect(timeSpanAnchor.id, ConstraintSet.BOTTOM, ConstraintSet.PARENT_ID, ConstraintSet.TOP)
+        constraintSet.constrainHeight(timeSpanAnchor.id, cardViewHandleDiameter / 2)
+
         constraintSet.constrainWidth(timeLine.id, ConstraintSet.WRAP_CONTENT)
         constraintSet.connect(timeLine.id, ConstraintSet.TOP, ConstraintSet.PARENT_ID, ConstraintSet.TOP)
 
@@ -175,7 +202,7 @@ class WeeklyRuleFragment : Fragment() {
             )
             constraintSet.connect(horizontalGuideLines[i].id, ConstraintSet.LEFT, timeLine.id, ConstraintSet.RIGHT)
             constraintSet.connect(horizontalGuideLines[i].id, ConstraintSet.RIGHT, ConstraintSet.PARENT_ID, ConstraintSet.RIGHT)
-            constraintSet.constrainHeight(horizontalGuideLines[i].id, lineWidth)
+            constraintSet.constrainHeight(horizontalGuideLines[i].id, guidelineWidth)
         }
 
         val verticalGuidelineChainIds = IntArray(verticalGuideLines.size)
@@ -184,7 +211,7 @@ class WeeklyRuleFragment : Fragment() {
         //constraining vertical guidelines
         for (i in verticalGuideLines.indices) {
             verticalGuidelineChainIds[i] = verticalGuideLines[i].id
-            constraintSet.constrainWidth(verticalGuideLines[i].id, lineWidth)
+            constraintSet.constrainWidth(verticalGuideLines[i].id, guidelineWidth)
             constraintSet.connect(verticalGuideLines[i].id, ConstraintSet.TOP, ConstraintSet.PARENT_ID, ConstraintSet.TOP)
             constraintSet.connect(verticalGuideLines[i].id, ConstraintSet.BOTTOM, ConstraintSet.PARENT_ID, ConstraintSet.BOTTOM)
         }
@@ -199,56 +226,115 @@ class WeeklyRuleFragment : Fragment() {
             ConstraintSet.CHAIN_SPREAD_INSIDE
         )
 
+        displayTimespans()
+        updateTimeIndicator(WeeklyTime.now())
+
         constraintSet.applyTo(timeLayout)
 
         val wrapContentMeasureSpec = View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED)
         timeLine.measure(wrapContentMeasureSpec, wrapContentMeasureSpec)
         daySpace.layoutParams.width = timeLine.measuredWidth
-        Log.d("UI", "timeLine.measuredWidth: ${timeLine.measuredWidth}")
-
-        displayTimespans()
-
-        if (weeklyRuleViewModel.initialCreation) {
-            weeklyRuleRiseDown()
-            weeklyRuleViewModel.initialCreation = false
-        } else {
-            rule.elevation = 0f
-        }
 
         return root
     }
 
     fun displayTimespans() {
         timeSpans.forEach { view -> timeLayout.removeView(view) }
-        val constraintSet = ConstraintSet()
-        constraintSet.clone(timeLayout)
 
         for (t in weeklyRuleViewModel.rule.timeSpans) {
-            Log.d("UI", "t.end.before(t.start): ${t.end.before(t.start)}")
-            if (t.start.before(t.end)) {
-                val card = EditTimeSpan(context!!)
-                card.elevation = elevation
-                timeLayout.addView(card)
-                timeSpans.add(card)
-
-                constraintSet.connect(card.id, ConstraintSet.LEFT, verticalGuideLines[t.start.day].id, ConstraintSet.RIGHT, timeSpanMargin)
-                constraintSet.connect(card.id, ConstraintSet.RIGHT, verticalGuideLines[t.start.day + 1].id, ConstraintSet.LEFT, timeSpanMargin)
-                constraintSet.connect(card.id, ConstraintSet.TOP, ConstraintSet.PARENT_ID, ConstraintSet.TOP, hourHeight * t.start.hour)
-                constraintSet.constrainHeight(card.id, (hourHeight * (t.end.inHours() - t.start.inHours())).toInt())
-            }
+            displayTimeSpan(t, constraintSet)
         }
 
         constraintSet.applyTo(timeLayout)
     }
 
-    fun weeklyRuleRiseDown() {
-        val riseDownAnimation = RiseDownAnimation(rule)
-        riseDownAnimation.duration = getAnimationDuration()
-        rule.startAnimation(riseDownAnimation)
+    fun displayTimeSpan(t: WeeklyTimeSpan, constraintSet: ConstraintSet) {
+        if (t.start.before(t.end)) {
+
+            for (i in t.start.day until t.end.day + 1) {
+                val card = createTimeSpan(i)
+
+                if (i > t.start.day) {
+                    constraintSet.connect(card.id, ConstraintSet.TOP, timeSpanAnchor.id, ConstraintSet.TOP)
+                } else {
+                    constraintSet.connect(card.id, ConstraintSet.TOP, ConstraintSet.PARENT_ID, ConstraintSet.TOP, (hourHeight * t.start.inDailyHours()).toInt())
+                }
+
+                if (i < t.end.day) {
+                    constraintSet.constrainHeight(card.id, hourHeight * 25)
+                } else if (i > t.start.day) {
+                    constraintSet.constrainHeight(card.id, (hourHeight * t.end.inDailyHours()).toInt() + cardViewHandleDiameter / 2)
+                } else {
+                    constraintSet.constrainHeight(card.id, (hourHeight * (t.end.inHours() - t.start.inHours())).toInt())
+                }
+            }
+        } else {
+            for (i in t.start.day until 7) {
+                val card = createTimeSpan(i)
+
+                if (i > t.start.day) {
+                    constraintSet.connect(card.id, ConstraintSet.TOP, timeSpanAnchor.id, ConstraintSet.TOP)
+                } else {
+                    constraintSet.connect(card.id, ConstraintSet.TOP, ConstraintSet.PARENT_ID, ConstraintSet.TOP, (hourHeight * t.start.inDailyHours()).toInt())
+                }
+
+                constraintSet.constrainHeight(card.id, hourHeight * 25)
+            }
+
+            for (i in 0 until t.end.day + 1) {
+                val card = createTimeSpan(i)
+
+                constraintSet.connect(card.id, ConstraintSet.TOP, timeSpanAnchor.id, ConstraintSet.TOP)
+
+                if (i < t.end.day) {
+                    constraintSet.constrainHeight(card.id, hourHeight * 25)
+                } else {
+                    constraintSet.constrainHeight(card.id, (hourHeight * t.end.inDailyHours()).toInt() + cardViewHandleDiameter / 2)
+                }
+            }
+        }
     }
 
-    private fun getAnimationDuration(): Long {
-        val key: String = MainActivity.res.getString(R.string.pref_animation_duration_key)
-        return MainActivity.getPrefInt(key, 250).toLong()
+    fun createTimeSpan(day: Int): View {
+        val card = View(context)
+        card.id = View.generateViewId()
+        card.background = ContextCompat.getDrawable(context!!, R.drawable.background_card_view)
+        card.elevation = cardViewElevation
+        timeLayout.addView(card)
+        timeSpans.add(card)
+
+        constraintSet.connect(card.id, ConstraintSet.LEFT, verticalGuideLines[day].id, ConstraintSet.RIGHT, timeSpanMargin)
+        constraintSet.connect(card.id, ConstraintSet.RIGHT, verticalGuideLines[day + 1].id, ConstraintSet.LEFT, timeSpanBigMargin)
+
+        return card
+    }
+
+    fun createEditTimeSpan(start: WeeklyTime): EditTimeSpan {
+        val card = EditTimeSpan(context!!)
+        card.elevation = cardViewHighElevation
+        timeLayout.addView(card)
+
+        constraintSet.connect(card.id, ConstraintSet.LEFT, verticalGuideLines[start.day].id, ConstraintSet.RIGHT, timeSpanMargin)
+        constraintSet.connect(card.id, ConstraintSet.RIGHT, verticalGuideLines[start.day + 1].id, ConstraintSet.LEFT, timeSpanMargin)
+        constraintSet.connect(card.id, ConstraintSet.TOP, timeSpanAnchor.id, ConstraintSet.TOP, (hourHeight * start.inDailyHours()).toInt())
+        constraintSet.constrainHeight(card.id, hourHeight)
+
+        return card
+    }
+
+    fun updateTimeIndicator(time: WeeklyTime) {
+        constraintSet.clear(timeIndicatorHead.id)
+        constraintSet.clear(timeIndicatorLine.id)
+
+        constraintSet.connect(timeIndicatorHead.id, ConstraintSet.RIGHT, verticalGuideLines[time.day].id, ConstraintSet.RIGHT)
+        constraintSet.connect(timeIndicatorHead.id, ConstraintSet.TOP, timeIndicatorLine.id, ConstraintSet.TOP)
+        constraintSet.connect(timeIndicatorHead.id, ConstraintSet.BOTTOM, timeIndicatorLine.id, ConstraintSet.BOTTOM)
+        constraintSet.constrainWidth(timeIndicatorHead.id, indicatorHeadDiameter)
+        constraintSet.constrainHeight(timeIndicatorHead.id, indicatorHeadDiameter)
+
+        constraintSet.connect(timeIndicatorLine.id, ConstraintSet.RIGHT, verticalGuideLines[time.day + 1].id, ConstraintSet.LEFT)
+        constraintSet.connect(timeIndicatorLine.id, ConstraintSet.LEFT, timeIndicatorHead.id, ConstraintSet.RIGHT)
+        constraintSet.connect(timeIndicatorLine.id, ConstraintSet.TOP, timeLayout.id, ConstraintSet.TOP, (hourHeight * time.inDailyHours()).toInt())
+        constraintSet.constrainHeight(timeIndicatorLine.id, indicatorLineWidth)
     }
 }
--- app/src/main/java/bedbrains/homesweethomeandroidclient/ui/rule/weeklyrule/WeeklyRuleViewModel.kt
+++ app/src/main/java/bedbrains/homesweethomeandroidclient/ui/rule/weeklyrule/WeeklyRuleViewModel.kt
@@ -10,11 +10,46 @@ class WeeklyRuleViewModel : ViewModel() {
     var initialCreation = true
 
     init {
-
         rule.timeSpans.add(
             WeeklyTimeSpan(
-                WeeklyTime(3, 5, 0, 0, 0),
-                WeeklyTime(3, 9, 15, 0, 0)
+                WeeklyTime(0, 13, 0, 0, 0),
+                WeeklyTime(0, 24, 0, 0, 0)
+            )
+        )
+        rule.timeSpans.add(
+            WeeklyTimeSpan(
+                WeeklyTime(1, 13, 0, 0, 0),
+                WeeklyTime(1, 24, 0, 0, 0)
+            )
+        )
+        rule.timeSpans.add(
+            WeeklyTimeSpan(
+                WeeklyTime(2, 13, 0, 0, 0),
+                WeeklyTime(2, 24, 0, 0, 0)
+            )
+        )
+        rule.timeSpans.add(
+            WeeklyTimeSpan(
+                WeeklyTime(3, 13, 0, 0, 0),
+                WeeklyTime(3, 24, 0, 0, 0)
+            )
+        )
+        rule.timeSpans.add(
+            WeeklyTimeSpan(
+                WeeklyTime(4, 13, 0, 0, 0),
+                WeeklyTime(5, 2, 0, 0, 0)
+            )
+        )
+        rule.timeSpans.add(
+            WeeklyTimeSpan(
+                WeeklyTime(5, 13, 0, 0, 0),
+                WeeklyTime(6, 2, 0, 0, 0)
+            )
+        )
+        rule.timeSpans.add(
+            WeeklyTimeSpan(
+                WeeklyTime(6, 13, 0, 0, 0),
+                WeeklyTime(6, 24, 0, 0, 0)
             )
         )
     }
